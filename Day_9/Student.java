32cd955e1c4a84ebcd2cb868905859caa0e6d8af
Suhail Ahmed <i.suhailahmed1@gmail.com>
2025-02-14 18:12:13 +0530

    Last Update

---
+++ Day_9/Student.java
@@ -0,0 +1,10 @@
+
+public class Student {
+    public static void main(String[] args) {
+            
+    }
+    void teching(){
+        System.out.println("Staff is teaching");
+    }
+    abstract void questionPaper();
+}
---
+++ Day_9/Teacher.java
@@ -0,0 +1,10 @@
+public abstract class Teacher { 
+    public static void main(String[] args) {
+        // Teacher t = new Teacher();
+        // can't able to create objs...
+
+    }
+    void teaching(){
+        System.out.println();
+    }
+}
